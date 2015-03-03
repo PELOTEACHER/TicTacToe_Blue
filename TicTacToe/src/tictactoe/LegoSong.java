659049957b8e0144a7b962b6cf646221ea164e30
favl <favl.dk@gmail.com>
2015-03-03 10:43:59 +0100

    New Class
    The LegoSong class has been created.

---
+++ TicTacToe/src/tictactoe/LegoSong.java
@@ -0,0 +1,66 @@
+package tictactoe;
+
+/**
+ *
+ * @author favl
+ */
+public class LegoSong {
+
+    /*
+     Melody: Everything is Awesome, from the Lego Movie.
+     */
+    public void singSong() {
+        System.out.println("Everything is Awesome, \n"
+                + "Everything is cool when you're part of a team\n"
+                + "Everything is Awesome when you're living out a dreams\n"
+                + "Everything is better when we stick together,\n"
+                + "Side by side you and I gonna win forever?\n"
+                + "Let's party forever\n"
+                + "We're the same unlike you, you & me we are a working in harmony\n"
+                + "Everything is Awesome, \n"
+                + "Everything is cool when you're part of a team\n"
+                + "Everything is Awesome when you're living out a dreams\n"
+                + "\n"
+                + "[Verse: The Lonely Island]\n"
+                + "I just heard the news\n"
+                + "Everyone's talking: Life is good!\n"
+                + "'Cause everything's awesome\n"
+                + "Awesome jobs and new opportunity\n"
+                + "More free time for my awesome community\n"
+                + "I feel more awesome than an awesome possum\n"
+                + "Put my body in chocolate frosting \n"
+                + "Three years later I shot the frosting \n"
+                + "Smelling like a possum, everything is awesome\n"
+                + "Accept to look at new brown shoes\n"
+                + "It's awesome to win and it's awesome to lose\n"
+                + "\n"
+                + "Everything is better when we stick together,\n"
+                + "Side by side you and I gonna win forever?\n"
+                + "Let's party forever\n"
+                + "We're the same unlike you, you & me we are workin' in harmony\n"
+                + "Everything is Awesome, \n"
+                + "Everything is cool when you're part of a team\n"
+                + "Everything is Awesome when you're living out a dreams\n"
+                + "\n"
+                + "Everything is Awesome, \n"
+                + "Everything is cool when you're part of a team\n"
+                + "Everything is Awesome when you're living out a dreams\n"
+                + "\n"
+                + "[Verse: The Lonely Island]\n"
+                + "Blue skies, bouncy springs, we just named a few awesome things\n"
+                + "A Nobel prize, a piece of string\n"
+                + "You know what's awesome? Everything \n"
+                + "Dogs with flees, allergies\n"
+                + "A book of Greek antiquities \n"
+                + "Brand new pants, a very old vest\n"
+                + "Awesome items are the best\n"
+                + "Things, dogs, clogs, they're awesome\n"
+                + "Rocks, clocks, and socks, they're awesome\n"
+                + "Figs, and wigs, and twigs, that's awesome\n"
+                + "Everything you see or think or say is awesome\n"
+                + "\n"
+                + "Everything is Awesome, \n"
+                + "Everything is cool when you're part of a team\n"
+                + "Everything is Awesome when you're living out a dreams");
+    }
+}
