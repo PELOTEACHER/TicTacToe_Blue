b0e741779a0bcbf44d5a271419138b953e7463bc
favl <favl.dk@gmail.com>
2015-03-03 10:20:20 +0100

    Merge branch 'master' of https://github.com/PELOTEACHER/TicTacToe_Blue
    Conflicts:
    TicTacToe/nbproject/private/private.properties

---
+++ TicTacToe/src/tictactoe/LegoSong.java
@@ -0,0 +1,24 @@
+package tictactoe;
+
+/**
+ *
+ * @author favl
+ */
+public class LegoSong {
+    
+    /*
+    Git git git git git git git.... Have you ever heard of copy / paste??
+    - No! Why do you ask???
+    
+    -HAHAHHHHAA
+   
+    
+    
+    ----------> Luke, I am your father!! <-------------
+    */
+    
+    public void everythingIsAwesome() {
+        System.out.println("Everything is awesome!!!!...........");
+    }
+    
+}
